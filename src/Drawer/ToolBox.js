d65f69928e4ad755845db206788cbbc7e42f97ac
Colin PUY <colinpuy.pro@gmail.com>
2019-04-04 12:31:24 +0200

    selected in toolbox

--- src/Drawer/ToolBox.js
+++ src/Drawer/ToolBox.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import tools from './tools';
 
 
-const Tool = ({ name, onSelect }) => <div onClick={() => onSelect(name)}>{name}</div>;
+const Tool = ({ name, onSelect, selected }) => <div onClick={() => onSelect(name)} className={selected.name === name ? 'setected' : ''}>{name}</div>;
 
 
 class ToolBox extends Component {
   render() {
     return (
       <div className="ToolBox">
-        {Object.keys(tools).map(name => <Tool key={name} name={name} onSelect={this.props.onSelect}/>)}
+        {Object.keys(tools).map(name => <Tool key={name} name={name} onSelect={this.props.onSelect} selected={this.props.selected}/>)}
       </div>
     );
   }
--- src/Drawer/index.js
+++ src/Drawer/index.js
@@ -8,21 +8,21 @@ class Drawer extends Component {
     super(props);
 
     this.state = {
-      tool: tools.stylo
+      tool: {...tools.stylo, name: 'stylo'}
     };
 
     this.handleToolSelect = this.handleToolSelect.bind(this);
   }
 
   handleToolSelect(name) {
-    this.setState({ tool: tools[name] });
+    this.setState({ tool: { ...tools[name], name }});
     console.log('selected tool: ', name)
   }
 
   render() {
     return (
       <>
-        <ToolBox onSelect={this.handleToolSelect}/>
+        <ToolBox onSelect={this.handleToolSelect} selected={this.state.tool}/>
         <DrawArea tool={this.state.tool}/>
       </>
     );
