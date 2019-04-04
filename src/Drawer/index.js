be3c843b3cea19686644c1f32657bfdf96514c88
Colin PUY <colinpuy.pro@gmail.com>
2019-04-04 11:56:28 +0200

    tool in index state

--- src/Drawer/index.js
+++ src/Drawer/index.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 import DrawArea from './DrawArea';
 import ToolBox from './ToolBox';
+import tools from './tools';
 
 class Drawer extends Component {
-  handleToolSelect(name){
-    console.log(name)
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tool: tools.stylo
+    };
+
+    this.handleToolSelect = this.handleToolSelect.bind(this);
+  }
+
+  handleToolSelect(name) {
+    this.setState({ tool: tools[name] });
+    console.log('selected tool: ', name)
   }
 
   render() {
     return (
       <>
         <ToolBox onSelect={this.handleToolSelect}/>
-        <DrawArea />
+        <DrawArea tool={this.state.tool}/>
       </>
     );
   }
